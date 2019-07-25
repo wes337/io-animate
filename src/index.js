12a4c3232ac92edce22058c17d687b901bf834bd
Wesley Moses <wesmoses@gmail.com>
2019-07-25 10:25:51 +0300

    takes cssPrefix and applies data-ioa attributes to elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
-import ioAnimate from './io-animate'
+require('intersection-observer')
 
-ioAnimate.init(
-  '.io-animate',
-  'fadeIn',
-  'fadeOut',
-  false,
-)
+function ioAnimate(
+  selector = '.io-animate',
+  cssPrefix = 'animated',
+  once = false
+) {
+  const intersectionObserver = new IntersectionObserver((entries, observer) => {
+    const animationIn = entry.target.getAttribute('data-ioa-in')
+    const animationOut = entry.target.getAttribute('data-ioa-out')
+    const delay = entry.target.getAttribute('data-ioa-delay')
+    const speed = entry.target.getAttribute('data-ioa-speed')
+
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        entry.target.classList.remove(cssPrefix, animationOut, delay, speed)
+        entry.target.classList.add(cssPrefix, animationIn, delay, speed)
+      } else if (entry.intersectionRatio !== 1) {
+        entry.target.classList.remove(cssPrefix, animationIn, delay, speed)
+        entry.target.classList.add(cssPrefix, animationOut), delay, speed
+      }
+
+      if (once) {
+        observer.unobserve(entry.target)
+      }
+    })
+  })
+  
+  const elements = [...document.querySelectorAll(selector)]
+  
+  elements.forEach((element) => intersectionObserver.observe(element))
+}
+
+module.exports = { ioAnimate }
